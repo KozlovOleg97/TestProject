72aa771908fc5be2808c93c9f6641f79aeb0d684
KozlovOleg97 <kozlovoleg2021@gmail.com>
2023-02-20 12:04:17 +0300

    Added Second Project

---
+++ TestProjectServices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestProjectServices.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
